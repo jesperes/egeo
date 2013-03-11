9e66eed02f9741f3406c4e7f863560e7ff48be0e
Jesper Eskilson <jesper.eskilson@iar.com>
2013-03-11 08:30:28 +0100

    Added proper cache icons

--- src/plugins/egeo.core/src/egeo/core/GpxLoader.java
+++ src/plugins/egeo.core/src/egeo/core/GpxLoader.java
@@ -13,53 +13,63 @@ import egeo.gpx.groundspeak.CacheType;
 import egeo.gpx.groundspeak.OwnerType;
 
 public class GpxLoader {
-	static EgeoFactory f = EgeoFactory.eINSTANCE;
+    static EgeoFactory f = EgeoFactory.eINSTANCE;
 
-	private static User getUser(OwnerType owner) {
-		User user = EgeoFactory.eINSTANCE.createUser();
-		user.setName(owner.getValue());
-		user.setUserId(Long.valueOf(owner.getId()));
-		return user;
-	}
+    private static User getUser(OwnerType owner) {
+        User user = EgeoFactory.eINSTANCE.createUser();
+        user.setName(owner.getValue());
+        user.setUserId(Long.valueOf(owner.getId()));
+        return user;
+    }
 
-	private static CacheType getGroundspeakExt(WptType wpt) {
-		if (wpt.getAny().size() > 0) {
-			Object any = wpt.getAny().get(0).getValue();
-			if (any instanceof CacheType) {
-				// has groundspeak extensions
-				return (CacheType) any;
-			}
-		}
+    private static CacheType getGroundspeakExt(WptType wpt) {
+        if (wpt.getAny().size() > 0) {
+            Object any = wpt.getAny().get(0).getValue();
+            if (any instanceof CacheType) {
+                // has groundspeak extensions
+                return (CacheType) any;
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	private static Coordinate getCoordinate(WptType wpt) {
-		Coordinate coord = f.createCoordinate();
-		coord.setLat(wpt.getLat());
-		coord.setLon(wpt.getLon());
-		return coord;
-	}
+    private static Coordinate getCoordinate(WptType wpt) {
+        Coordinate coord = f.createCoordinate();
+        coord.setLat(wpt.getLat());
+        coord.setLon(wpt.getLon());
+        return coord;
+    }
 
-	public static Collection<Geocache> loadGpx(GpxType gpx) {
+    public static Collection<Geocache> loadGpx(GpxType gpx) {
 
-		ArrayList<Geocache> list = new ArrayList<Geocache>();
+        ArrayList<Geocache> list = new ArrayList<Geocache>();
 
-		for (WptType wpt : gpx.getWpt()) {
-			Geocache cache = f.createGeocache();
-			list.add(cache);
+        for (WptType wpt : gpx.getWpt()) {
+            Geocache cache = f.createGeocache();
+            list.add(cache);
 
-			cache.setCode(wpt.getName());
-			cache.setCoord(getCoordinate(wpt));
+            cache.setCode(wpt.getName());
+            cache.setCoord(getCoordinate(wpt));
 
-			CacheType gscache = getGroundspeakExt(wpt);
-			if (gscache != null) {
-				cache.setName(gscache.getName());
-				cache.setPlacedBy(gscache.getPlacedBy());
-				cache.setOwner(getUser(gscache.getOwner().get(0)));
-			}
-		}
+            CacheType gscache = getGroundspeakExt(wpt);
+            if (gscache != null) {
+                cache.setName(gscache.getName());
+                cache.setPlacedBy(gscache.getPlacedBy());
+                cache.setOwner(getUser(gscache.getOwner().get(0)));
 
-		return list;
-	}
+                String type = gscache.getType();
+                if (type.equals("Traditional Cache"))
+                    cache.setCacheType(egeo.CacheType.TRADITIONAL);
+                else if (type.equals("Unknown Cache"))
+                    cache.setCacheType(egeo.CacheType.UNKNOWN);
+                else if (type.equals("Multi-cache"))
+                    cache.setCacheType(egeo.CacheType.MULTI);
+                else if (type.equals("Event Cache"))
+                    cache.setCacheType(egeo.CacheType.EVENT);
+            }
+        }
+
+        return list;
+    }
 }
--- src/plugins/egeo.ui/src/egeo/ui/MapView.java
+++ src/plugins/egeo.ui/src/egeo/ui/MapView.java
@@ -16,6 +16,7 @@ import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
@@ -30,92 +31,116 @@ import egeo.ui.MapWidget.PointD;
 
 public class MapView extends ViewPart {
 
-	private MapWidget mapWidget;
-	private Collection<Geocache> caches = Collections.emptyList();
-	private final Collection<Geocache> visibleCaches = new ArrayList<Geocache>();
-	Coordinate center;
-	// String gpx = "GC3X94J.gpx";
-	String gpx = "5110831.gpx";
+    private MapWidget mapWidget;
+    private Collection<Geocache> caches = Collections.emptyList();
+    private final Collection<Geocache> visibleCaches = new ArrayList<Geocache>();
+    Coordinate center;
+    // String gpx = "GC3X94J.gpx";
+    String gpx = "5982344_Uppsala.gpx";
+    // String gpx = "5110831.gpx";
+    private final CacheIconFactory iconFactory;
 
-	public MapView() {
-		ResourceSet resourceSet = new ResourceSetImpl();
-		URI uri = URI.createPlatformPluginURI("/egeo.ui/" + gpx, false);
-		Resource resource = resourceSet.createResource(uri);
-		Map<String, Object> map = new HashMap<String, Object>();
-		try {
-			resource.load(map);
-			DocumentRoot root = (DocumentRoot) resource.getContents().get(0);
-			caches = GpxLoader.loadGpx(root.getGpx());
-			Geocache cache = caches.iterator().next();
-			center = cache.getCoord();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public MapView() {
+        iconFactory = CacheIconFactory.getInstance();
 
-	@Override
-	public void createPartControl(Composite parent) {
-		mapWidget = new MapWidget(parent, SWT.NONE);
-		mapWidget.addPaintListener(new PaintListener() {
-			@Override
-			public void paintControl(PaintEvent e) {
-				Point mapPosition = mapWidget.getMapPosition();
-				for (Geocache cache : visibleCaches) {
-					Coordinate coord = cache.getCoord();
-					PointD point = new PointD(coord.getLon().doubleValue(),
-							coord.getLat().doubleValue());
-					Point cachePos = mapWidget.computePosition(point);
+        ResourceSet resourceSet = new ResourceSetImpl();
+        URI uri = URI.createPlatformPluginURI("/egeo.ui/" + gpx, false);
+        Resource resource = resourceSet.createResource(uri);
+        Map<String, Object> map = new HashMap<String, Object>();
+        try {
+            resource.load(map);
+            DocumentRoot root = (DocumentRoot) resource.getContents().get(0);
+            caches = GpxLoader.loadGpx(root.getGpx());
+            Geocache cache = caches.iterator().next();
+            center = cache.getCoord();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-					Point p = new Point(cachePos.x - mapPosition.x, cachePos.y
-							- mapPosition.y);
-					e.gc.setBackground(Display.getCurrent().getSystemColor(
-							SWT.COLOR_RED));
-					e.gc.fillRectangle(p.x - 5, p.y - 5, 10, 10);
+    @Override
+    public void createPartControl(Composite parent) {
+        mapWidget = new MapWidget(parent, SWT.NONE);
+        mapWidget.addPaintListener(new PaintListener() {
+            @Override
+            public void paintControl(PaintEvent e) {
+                Point mapPosition = mapWidget.getMapPosition();
+                for (Geocache cache : visibleCaches) {
+                    Coordinate coord = cache.getCoord();
+                    PointD point = new PointD(coord.getLon().doubleValue(),
+                            coord.getLat().doubleValue());
+                    Point cachePos = mapWidget.computePosition(point);
 
-					e.gc.setBackground(Display.getCurrent().getSystemColor(
-							SWT.COLOR_WHITE));
-					e.gc.setForeground(Display.getCurrent().getSystemColor(
-							SWT.COLOR_BLACK));
-					e.gc.drawText(cache.getName(), p.x, p.y);
-				}
-			}
-		});
+                    Point p = new Point(cachePos.x - mapPosition.x, cachePos.y
+                            - mapPosition.y);
 
-		mapWidget.addPropertyChangeListener(new PropertyChangeListener() {
-			@Override
-			public void propertyChange(PropertyChangeEvent arg0) {
-				// System.out.println(arg0.getPropertyName() + ": "
-				// + arg0.getNewValue());
-				if (!arg0.getPropertyName().equals("mapPosition"))
-					return;
+                    Image image = iconFactory.getIcon(cache.getCacheType());
+                    if (image != null) {
+                        Rectangle bounds = image.getBounds();
 
-				Point mapPosition = (Point) arg0.getNewValue();
+                        e.gc.setForeground(Display.getCurrent().getSystemColor(
+                                SWT.COLOR_BLACK));
+                        e.gc.setBackground(Display.getCurrent().getSystemColor(
+                                SWT.COLOR_WHITE));
 
-				visibleCaches.clear();
+                        int margin = 2;
+                        int rounded = 6;
+                        Rectangle frame = new Rectangle( //
+                                p.x - bounds.width / 2 - margin, //
+                                p.y - bounds.height / 2 - margin, //
+                                bounds.width + margin * 2 - 1, //
+                                bounds.height + margin * 2 - 1 //
+                        );
 
-				for (Geocache cache : caches) {
-					Coordinate coord = cache.getCoord();
-					PointD point = new PointD(coord.getLon().doubleValue(),
-							coord.getLat().doubleValue());
-					Point cachePos = mapWidget.computePosition(point);
+                        e.gc.fillRoundRectangle(frame.x, frame.y, frame.width,
+                                frame.height, rounded, rounded);
 
-					Point mapSize = mapWidget.getSize();
-					Rectangle visibleMap = new Rectangle(mapPosition.x,
-							mapPosition.y, mapSize.x, mapSize.y);
+                        e.gc.drawRoundRectangle(frame.x, frame.y, frame.width,
+                                frame.height, rounded, rounded);
 
-					if (visibleMap.contains(cachePos)) {
-						visibleCaches.add(cache);
-					}
-				}
-			}
-		});
-	}
+                        e.gc.drawImage(image, p.x - bounds.width / 2, p.y
+                                - bounds.height / 2);
 
-	@Override
-	public void setFocus() {
-		mapWidget.setFocus();
-		Point pos = mapWidget.computePosition(new PointD(center.getLon()
-				.doubleValue(), center.getLat().doubleValue()));
-		mapWidget.setCenterPosition(pos);
-	}
+                    }
+                }
+            }
+        });
+
+        mapWidget.addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent arg0) {
+                // System.out.println(arg0.getPropertyName() + ": "
+                // + arg0.getNewValue());
+                if (!arg0.getPropertyName().equals("mapPosition"))
+                    return;
+
+                Point mapPosition = (Point) arg0.getNewValue();
+
+                visibleCaches.clear();
+
+                for (Geocache cache : caches) {
+                    Coordinate coord = cache.getCoord();
+                    PointD point = new PointD(coord.getLon().doubleValue(),
+                            coord.getLat().doubleValue());
+                    Point cachePos = mapWidget.computePosition(point);
+
+                    Point mapSize = mapWidget.getSize();
+                    Rectangle visibleMap = new Rectangle(mapPosition.x,
+                            mapPosition.y, mapSize.x, mapSize.y);
+
+                    if (visibleMap.contains(cachePos)) {
+                        visibleCaches.add(cache);
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void setFocus() {
+        mapWidget.setFocus();
+        Point pos = mapWidget.computePosition(new PointD(center.getLon()
+                .doubleValue(), center.getLat().doubleValue()));
+        mapWidget.setCenterPosition(pos);
+    }
 }
